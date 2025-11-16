0aa9db5ce35188faab885e3c2f1934666b85c3da
Varrahan <varrahan@gmail.com>
2025-11-16 00:29:57 -0500

    Add memory program file

---
+++ src/memory.c
@@ -0,0 +1,48 @@
+#include "memory.h"
+#include "architecture.h"
+#include "terminal.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+// Write value to memory
+void mem_write(uint16_t address, uint16_t val) {
+    memory[address] = val;
+}
+
+// Read value from memory, handling memory-mapped I/O
+uint16_t mem_read(uint16_t address) {
+    if (address == MR_KBSR) {
+        if (check_key()) {
+            memory[MR_KBSR] = (1 << 15);
+            memory[MR_KBDR] = getchar();
+        } else {
+            memory[MR_KBSR] = 0;
+        }
+    }
+    return memory[address];
+}
+
+// Read image file into memory
+void read_image_file(FILE* file) {
+    uint16_t origin;
+    fread(&origin, sizeof(origin), 1, file);
+    origin = swap16(origin);
+
+    uint16_t max_read = MEMORY_MAX - origin;
+    uint16_t* p = memory + origin;
+    size_t read = fread(p, sizeof(uint16_t), max_read, file);
+
+    while (read-- > 0) {
+        *p = swap16(*p);
+        ++p;
+    }
+}
+
+// Load image from file path
+int read_image(const char* image_path) {
+    FILE* file = fopen(image_path, "rb");
+    if (!file) { return 0; };
+    read_image_file(file);
+    fclose(file);
+    return 1;
+}

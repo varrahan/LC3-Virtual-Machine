79e7bb25a9640e3c0b55ba687ca510db595d5345
Varrahan <varrahan@gmail.com>
2025-11-17 01:37:47 -0500

    Update architecture to support interrupts

--- src/architecture.c
+++ src/architecture.c
@@ -1,6 +1,6 @@
 #include "architecture.h"
 
-// Global memory and registers */
+// Global memory and registers
 uint16_t memory[MEMORY_MAX];
 uint16_t reg[R_COUNT];
 
@@ -16,10 +16,13 @@ uint16_t sign_extend(uint16_t x, int bit_count) {
 void update_flags(uint16_t r) {
     if (reg[r] == 0) {
         reg[R_COND] = FL_ZRO;
+        reg[R_PSR] = (reg[R_PSR] & ~0x7) | FL_ZRO;
     } else if (reg[r] >> 15) {
         reg[R_COND] = FL_NEG;
+        reg[R_PSR] = (reg[R_PSR] & ~0x7) | FL_NEG;
     } else {
         reg[R_COND] = FL_POS;
+        reg[R_PSR] = (reg[R_PSR] & ~0x7) | FL_POS;
     }
 }
 
